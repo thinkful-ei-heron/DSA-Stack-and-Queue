04aa7de55b0bd26ea79b4468c56e3445111516bf
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-12-13 12:48:23 -0800

    matching state version 1 working

--- stackClass.js
+++ stackClass.js
@@ -14,16 +14,32 @@ class Stack {
         if (this.top === null) {
             let node = new Node(item, null);
             this.top = node;
-        }
+        } else {
         let node = new Node(item, this.top);
         this.top = node;
+        }
     }
     pop() {
         const node = this.top;
         this.top = node.next;
-        return node.data;
+        return node.data; 
     }
 }
+// const node = this.top;
+// if(node === null) {
+//     this.top = null;
+//     //console.log('This is empty');
+//     return;
+// } else {
+//     this.top = node.next;
+//     return node.data;
+// }
+
+// pop() {
+//     const node = this.top;
+//     this.top = node.next;
+//     return node.data;
+// }
 
 function display(stk) {
     let currNode = stk.top;
@@ -35,14 +51,17 @@ function display(stk) {
     return;
 }
 
-
 function isEmpty(stack) {
-    if (stack.top === null) return true;
-    return false;
+    console.log(stack)
+    if (stack.top === null) {
+        return true;
+    } else return false;
 }
+
 function peek(stack) {
-    if (stack.top === null) return null;
-    return stack.top.data;
+    if (stack.top === null) {
+        return null;
+    } else return stack.top.data;
 }
 
 function is_palindrome(s) {
@@ -62,56 +81,59 @@ function is_palindrome(s) {
 // console.log(is_palindrome('1001'));
 // console.log(is_palindrome('Tauhida'));
 
-// () <-- true
-// ((()))} <--- false
-// (( ( )(( ))( ) ))
-// console.log('(' < ')');
-// console.log('[' < ']');
-// console.log('{' < '}');
-// iterate over the string create a stack from characters that match "(" or ")"
-// concate into a string
-// 
+// {()(())()}
+// () {} []
+// stack.push({);
+// stack.push((); 
+// ( , {
+// ) 
+// stack.pop()
+// {
+// stack.push(()
+// stack.push(()
+// ( ( {
+// stack
 
-// function matchingChar(s){
-//     let reversed = new Stack();
-    
-//     let compare = '';
-//     let specChars = ['(',')','[',']','{','}'];
-//     for(let i = 0; i < s.length; i++) {
-//         if(specChars.includes(s[i])){
-//            reversed.push(s[i]);
-//            compare += s[i];
-//         }
-//     }
-//     if(reversed.top === null) return false;
-// console.log(compare);
-// for(let i=0; i<compare.length; i++) {
-//     if(compare[i] > compare[i+1]) {
-//         return i
-//     } 
-// }
-// }
+
 function matchingChar(str) {
     let stack = new Stack();
-    if (str.length <= 1) return false;
-    let matchingOpeningBracket, currChar;
-    let openingBrackets = ['[', '{', '('];
-    let closingBrackets = [']', '}', ')'];
-    for (let i = 0; i < str.length; i++) {
-        currChar = str[i];
-   // if(openingBrackets.includes(currChar) || closingBrackets.includes(currChar)) {
-      if (closingBrackets.indexOf(currChar) > -1) {
-        matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(currChar)];
-        if (stack.top === null || (stack.pop() != matchingOpeningBracket)) return {value:false, pos:i};
-      } else stack.push(currChar);
-        //}
+    let openChar = '([{';
+    let closeChar = ')]}';
+    let index = 0;
+    for(let i = 0; i < str.length; i++) {
+        if (openChar === str[i]) {
+            index = i
+            stack.push('(');
+        }
+        else if (str[i] === closeChar) {
+            if(!peek(stack)){
+                //console.log(i)
+                return false;
+            }
+            stack.pop();
+        }
+        // else if (closeChar.includes(str[i]) && peek(stack) === null) {
+        //     return false;
+        // }
+   }
+    if(isEmpty(stack)){
+        return true;
+    } else {
+        console.log(index);
+        return false;
     }
-    return !(stack.top === null);
+   // console.log('final')
+    // if(isEmpty(stack)) {
+    //     console.log
+    //     return true;
+    // } else if (!isEmpty(stack)) return false;
 }
+// ())
   
-let string01 = 'this(has) the same(num) of (parens)';
-let string = '{}}';
-console.log(matchingChar(string01));
+let string01 = 'this()has the same(((num)) of (parens)';
+let breakmaybe = '())'
+let string = 'x) (hello)';
+console.log(matchingChar(breakmaybe));
 
 // outside matched = true;
 // position = 0;
@@ -125,9 +147,35 @@ function main() {
     starTrek.push('Spock');
     starTrek.push('McCoy');
     starTrek.push('Scotty');
+    starTrek.pop();
     //display(starTrek);
    // isEmpty(starTrek);
 }
 
 
 main();
+
+// function matchingChar(str) {
+//     let stack = new Stack();
+//     let openChar = '(';
+//     let closeChar = ')';
+//     for(let i = 0; i < str.length; i++) {
+//         if (openChar.includes(str[i])) {
+//             stack.push(str[i]);
+//         }
+//         else if (openChar.indexOf(peek(stack)) === closeChar.indexOf(str[i])) { // <-- finding the pairs not 100%
+//             stack.pop();
+//         }
+//         else if (closeChar.includes(str[i]) && peek(stack) === null) {
+//             return false;
+//         }
+//     }
+//     console.log(stack);
+//     display(stack);
+//     return;
+//    // console.log('final')
+//     // if(isEmpty(stack)) {
+//     //     console.log
+//     //     return true;
+//     // } else if (!isEmpty(stack)) return false;
+// }
